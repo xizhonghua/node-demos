66a52ac06bc071a10773891736b39441002d8dde
Zhonghua Xi <xizhonghua@gmail.com>
2014-12-14 15:37:12 -0500

    update

--- hello_http.js
+++ hello_http.js
@@ -1,8 +1,16 @@
 var http = require('http');
 
-var server = http.createServer(function(req, res) {
-    res.writeHead(200);
-    res.end('Hello Http\n');
-});
+function request_handler(req, res) {
+    var body = "Hey, thanks for calling!";
+    var content_length = body.length;
+    res.writeHead(200, {
+        'Content-Type' : 'text/plain',
+        'Content-Length' : content_length
+    });
+
+    res.end(body);
+}
+
+var server = http.createServer(request_handler);
 
 server.listen(8080);

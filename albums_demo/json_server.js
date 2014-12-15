72647992d0fcfd9af22fab119af306dbc7f267f9
Zhonghua Xi <xizhonghua@gmail.com>
2014-12-14 19:14:04 -0500

    support querystring an paging

--- albums_demo/json_server.js
+++ albums_demo/json_server.js
@@ -1,4 +1,5 @@
 var http = require('http');
+var url = require('url');
 var fs = require('fs');
 
 //  /albums.json
@@ -23,9 +24,22 @@ function handle_load_albums(req, res) {
 
 function handle_get_album(req, res) {
 
-    var album_name = req.url.substr(7, req.url.length - 12);
+    var core_url = req.parsed_url.pathname;
+    var album_name = core_url.substr(8, core_url.length - 13);
+    var query = req.parsed_url.query;
 
-    load_album(album_name, function(err, photos) {
+    
+
+    var page = parseInt(query.page);
+    var page_size = parseInt(query.page_size);
+
+    if(isNaN(page) || page < 0 ) page = 0;
+    if(isNaN(page_size) || page_size < 0 ) page_size = 20;
+
+    console.log('handle_get_album album_name = ' + album_name
+     + ' page = ' + page + ' page_size = ' + page_size);
+
+    load_album(album_name, page, page_size, function(err, photos) {
         if(err != null) {
             res.writeHead(503, {
                 'Content-Type' : 'application/json'
@@ -43,9 +57,16 @@ function handle_get_album(req, res) {
 function handle_request(req, res){
     console.log('Incoming request: (' + req.method + ')' + req.url);
 
-    if(req.url == '/albums.json') {
+    req.parsed_url = url.parse(req.url, true);
+    
+    var core_url = req.parsed_url.pathname;
+    var query = req.parsed_url.query;
+    console.log(core_url);
+    console.log(query);
+
+    if(core_url == '/albums.json') {
         handle_load_albums(req, res);
-    } else if(/albums\/.+\.json/.test(req.url)) {
+    } else if(/albums\/.+\.json/.test(core_url)) {
         handle_get_album(req, res);
     } else {
         res.writeHead(404, { 'Content-Type' : 'application/json'});
@@ -83,7 +104,7 @@ function load_album_list(callback){
     });
 }
 
-function load_album(album_name, callback){
+function load_album(album_name, page, page_size, callback){
     fs.readdir('albums/' + album_name, function(err, file_list){
         if(err) {
             callback(err);
@@ -94,7 +115,8 @@ function load_album(album_name, callback){
 
         (function iterator(i) {
             if(i >= file_list.length) {
-                callback(null, files_only);
+                var photos = files_only.splice(page * page_size, page_size);
+                callback(null, photos);
                 return;
             }
 
